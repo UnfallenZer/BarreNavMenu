20f10aa39915c7f7beef1799456261a03c6f5ac3
UnfallenZer <88389341+UnfallenZer@users.noreply.github.com>
2021-09-28 17:01:34 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+
+    const btn = document.querySelector('.bouton-click');
+    const menuDeroul = document.querySelector('.menuDeroulant');
+
+
+        btn.addEventListener('click', transformation)
+
+        function transformation(){
+
+            menuDeroul.classList.toggle('active')
+
+        }
+
+        
+
+
